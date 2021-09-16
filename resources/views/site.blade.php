204318393077b4234e710e4f6b556c4eee9ae707
Lucas Figueiredo <lukas.liberato14@gmail.com>
2021-09-16 20:47:28 -0300

    layout da pagina principal

--- resources/views/site.blade.php
+++ resources/views/site.blade.php
@@ -2,7 +2,78 @@
 
 @section('content')
     <pagina tamanho="12">
-        
+        <painel titulo="Artigos">
+
+            <div class="row">
+                <div class="col-sm-6 col-md-4">
+                  <div class="thumbnail">
+                    <img src="https://cdn.pixabay.com/photo/2017/09/18/16/54/links-2762389_960_720.png" alt="...">
+                    <div class="caption">
+                      <h3>Thumbnail label</h3>
+                      <p>Description</p>
+                      <p><a href="#" class="btn btn-primary" role="button">Leia mais</a></p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-sm-6 col-md-4">
+                  <div class="thumbnail">
+                    <img src="https://cdn.pixabay.com/photo/2017/09/18/16/54/links-2762389_960_720.png" alt="...">
+                    <div class="caption">
+                      <h3>Thumbnail label</h3>
+                      <p>Description</p>
+                      <p><a href="#" class="btn btn-primary" role="button">Leia mais</a></p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-sm-6 col-md-4">
+                  <div class="thumbnail">
+                    <img src="https://cdn.pixabay.com/photo/2017/09/18/16/54/links-2762389_960_720.png" alt="...">
+                    <div class="caption">
+                      <h3>Thumbnail label</h3>
+                      <p>Description</p>
+                      <p><a href="#" class="btn btn-primary" role="button">Leia mais</a></p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-sm-6 col-md-4">
+                  <div class="thumbnail">
+                    <img src="https://cdn.pixabay.com/photo/2017/09/18/16/54/links-2762389_960_720.png" alt="...">
+                    <div class="caption">
+                      <h3>Thumbnail label</h3>
+                      <p>Description</p>
+                      <p><a href="#" class="btn btn-primary" role="button">Leia mais</a></p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-sm-6 col-md-4">
+                  <div class="thumbnail">
+                    <img src="https://cdn.pixabay.com/photo/2017/09/18/16/54/links-2762389_960_720.png" alt="...">
+                    <div class="caption">
+                      <h3>Thumbnail label</h3>
+                      <p>Description</p>
+                      <p><a href="#" class="btn btn-primary" role="button">Leia mais</a></p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-sm-6 col-md-4">
+                  <div class="thumbnail">
+                    <img src="https://cdn.pixabay.com/photo/2017/09/18/16/54/links-2762389_960_720.png" alt="...">
+                    <div class="caption">
+                      <h3>Thumbnail label</h3>
+                      <p>Description</p>
+                      <p><a href="#" class="btn btn-primary" role="button">Leia mais</a></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+        </painel>
     </pagina>
+    
 @endsection
 
